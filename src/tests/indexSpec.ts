3df4835f58f7271268373cc9be6ab901033c0530
mahmoudfat7i <devmahmoudfathy@gmail.com>
2022-12-24 19:28:30 +0200

    add test with valid data

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -24,6 +24,14 @@ it('tests the image endpoint with missing data', async () => {
   const response = await request.get('/api/images')
   expect(response.status).toBe(400)
 })
+it('test image processing function with valid parameters', () => {
+  const correctDataParameters = getImage({
+    filename: 'fjord',
+    width: 100 as unknown as string,
+    height: 100 as unknown as string,
+  })
+  expect(correctDataParameters).toBeTruthy
+})
 it('test image processing function with invalid data', () => {
   const correctDataParameters = getImage({ filename: 'mjm' })
   expect(correctDataParameters).toThrowError
